2d175ce76392bb3f698f7f816853480c0c75712e
G3nK3n <ken_serrano@Hotmail.com>
2022-09-20 23:44:09 -0400

    Fixes on Signup Page

--- react-login/src/Components/Form/Form.js
+++ react-login/src/Components/Form/Form.js
@@ -1,34 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from './Form.module.css';
 
 import Button from 'react-bootstrap/Button';
 import Forms from 'react-bootstrap/Form';
 
 const Form = () => {
+
+    const [username, getUsername] = useState("");
+    const [isEmpty, getIsEmpty] = useState(true);
+
+    /* FIX NON-BLANK INPUT WHEN ERASING EVERYTHING */
+    const checkUsername = (e) => {
+        getUsername(e.target.value.trim());
+        if(username.length < 0) {
+            getIsEmpty(true);
+        }
+        else {
+            getIsEmpty(false);
+        }
+    }
+
     return (
         <div className={classes.Form}>
             <div className={classes.LoginText}>
                 <h1>Login Required</h1>
             </div>
             <Forms>
-                <Forms.Group controlId="formBasicName">
-                    <Forms.Label>Your Name</Forms.Label>
-                    <Forms.Control type="name" />
+                <Forms.Group className={classes.Name} controlId="formBasicName">
+                    <Forms.Label>Your Name</Forms.Label> <br />
+                    <Forms.Control onChange={theChange => checkUsername(theChange)} type="name" />
                 </Forms.Group>
-                <Forms.Group controlId="formBasicEmail">
-                    <Forms.Label>Email</Forms.Label>
+                <div className={classes.NameText}>
+                    {/* Add a " username=="" " to check if there is whitespace? It works after erasing the input */}
+                    <p>{isEmpty || username==""? "Please enter a name." : " "}</p>
+                </div>
+                <Forms.Group className={classes.Email} controlId="formBasicEmail">
+                    <Forms.Label>Email</Forms.Label> <br />
                     <Forms.Control type="email" />
                 </Forms.Group>
-                <Forms.Group controlId="formBasicPassword">
-                    <Forms.Label>Password</Forms.Label>
+                <Forms.Group className={classes.Password} controlId="formBasicPassword">
+                    <Forms.Label>Password</Forms.Label> <br />
                     <Forms.Control type="password" />
                 </Forms.Group>
-                <Button variant="primary" type="button">
-                    Signup
-                </Button>
-                <Button variant="primary" type="button">
-                    Switch to Login
-                </Button>
+                <div className={classes.ButtonGroup}>
+                    <Button className={classes.SignupButton} variant="primary" type="button">
+                        Signup
+                    </Button>
+                    <br />
+                    <Button className={classes.SwitchLoginButton} variant="primary" type="button">
+                        Switch to Login
+                    </Button>
+                </div>
             </Forms>
         </div>
     )
