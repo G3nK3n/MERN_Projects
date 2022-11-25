1fe5c9f4db6d192bd998fe61e8837fdb8e6087b3
G3nK3n <ken_serrano@Hotmail.com>
2022-11-24 23:41:53 -0500

    Fixed swtiching forms when LoggedIn

--- react-login/src/Components/Form/Login.js
+++ react-login/src/Components/Form/Login.js
@@ -15,7 +15,10 @@ const Login = () => {
 
     const switchHandler = () => {
         setIsSwitched(!isSwitched)
-        console.log(isSwitched);
+        setErrors({})
+        setForm({})
+        //FIGURE OUT HOW TOP CLEAR FORM AFTER SWITCHING
+        
     }
 
     const setField = (field, value) => {
@@ -34,9 +37,13 @@ const Login = () => {
     }
 
     const validateForm = () => {
-        const {email, password} = form;
+        const {name, email, password} = form;
 
         const newErrors = {}
+
+        if(!name || name === '') {
+            newErrors.name = "Please enter your name";
+        }
 
         if(!email || email === '') {
             newErrors.email = "Please enter an email";
@@ -71,7 +78,15 @@ const Login = () => {
                 <div className={classes.LoginText}>
                     <h1>Login Required</h1>
                 </div>
+                {isSwitched ? 
                 <Forms>
+                    <Forms.Group className={classes.Name} controlId="formBasicName">
+                        <Forms.Label>Name</Forms.Label> <br />
+                        <Forms.Control onChange={(e) => setField('name', e.target.value)} isInvalid={!!errors.name} name="name" required type="name" />
+                    </Forms.Group>
+                    <Forms.Control.Feedback type='invalid'>
+                        {errors.name}
+                    </Forms.Control.Feedback>
                     <Forms.Group className={classes.Email} controlId="formBasicEmail">
                         <Forms.Label>E-Mail</Forms.Label> <br />
                         <Forms.Control onChange={(e) => setField('email', e.target.value)} isInvalid={!!errors.email} name="email" required type="email" />
@@ -86,16 +101,32 @@ const Login = () => {
                     <Forms.Control.Feedback type='invalid'>
                         {errors.password}
                     </Forms.Control.Feedback>
-                    <div className={classes.ButtonGroup}>
-                        <Button onClick={handleSubmit} className={classes.SignupButton} variant="primary" type="button">
-                            Login
-                        </Button>
-                        <br />
-                        <Button onClick={switchHandler} className={classes.SignupButton2} type="button">
-                            Switch To Signup
-                        </Button>
-                    </div>  
-                </Forms>
+                </Forms>  
+                : <Forms>
+                        <Forms.Group className={classes.Email} controlId="formBasicEmail">
+                            <Forms.Label>E-Mail</Forms.Label> <br />
+                            <Forms.Control onChange={(e) => setField('email', e.target.value)} isInvalid={!!errors.email} name="email" required type="email" />
+                        </Forms.Group>
+                        <Forms.Control.Feedback type='invalid'>
+                            {errors.email}
+                        </Forms.Control.Feedback>
+                        <Forms.Group className={classes.Password} controlId="formBasicPassword">
+                            <Forms.Label>Password</Forms.Label> <br />
+                            <Forms.Control name="password" onChange={(e) => setField('password', e.target.value)} isInvalid={!!errors.password} required type="password" />
+                        </Forms.Group>
+                        <Forms.Control.Feedback type='invalid'>
+                            {errors.password}
+                        </Forms.Control.Feedback>
+                    </Forms>}
+                <div className={classes.ButtonGroup}>
+                    <Button onClick={handleSubmit} className={classes.SignupButton} variant="primary" type="button">
+                        {isSwitched ? 'Signup' : 'Login'}
+                    </Button>
+                    <br />
+                    <Button onClick={switchHandler} className={classes.SignupButton2} type="button">
+                        {isSwitched ? 'Switch to Login' : 'Switch to Signup'}
+                    </Button>
+                </div>  
             </div>
         </div>
     )
